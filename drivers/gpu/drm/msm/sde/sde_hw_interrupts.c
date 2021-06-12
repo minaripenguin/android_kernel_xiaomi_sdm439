35f0f4300e85414591a7b5406f666320a3fc2035
Sultan Alsawaf <sultan@kerneltoast.com>
2021-06-13 07:50:44 +0800

    drm/msm/sde: Remove redundant write memory barriers from IRQ routines
    Explicit write memory barriers are unneeded here since releasing a lock
    already implies a full memory barrier.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
+++ drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
@@ -849,8 +849,9 @@ static void sde_hw_intr_dispatch_irq(struct sde_hw_intr *intr,
 				if (cbfunc)
 					cbfunc(arg, irq_idx);
 				else
-					intr->ops.clear_intr_status_nolock(
-							intr, irq_idx);
+					SDE_REG_WRITE(&intr->hw,
+						intr->sde_irq_tbl[reg_idx].clr_off,
+						sde_irq_map[irq_idx].irq_mask);
 
 				/*
 				 * When callback finish, clear the irq_status
@@ -896,9 +897,6 @@ static int sde_hw_intr_enable_irq(struct sde_hw_intr *intr, int irq_idx)
 		SDE_REG_WRITE(&intr->hw, reg->clr_off, irq->irq_mask);
 		/* Enabling interrupts with the new mask */
 		SDE_REG_WRITE(&intr->hw, reg->en_off, cache_irq_mask);
-
-		/* ensure register write goes through */
-		wmb();
 
 		intr->cache_irq_mask[reg_idx] = cache_irq_mask;
 	}
@@ -957,6 +955,7 @@ static int sde_hw_intr_disable_irq_nolock(struct sde_hw_intr *intr, int irq_idx)
 static int sde_hw_intr_disable_irq(struct sde_hw_intr *intr, int irq_idx)
 {
 	unsigned long irq_flags;
+	int reg_idx;
 
 	if (!intr)
 		return -EINVAL;
@@ -1037,6 +1036,12 @@ static void sde_hw_intr_get_interrupt_statuses(struct sde_hw_intr *intr)
 	if (!intr)
 		return;
 
+	reg_idx = sde_irq_map[irq_idx].reg_idx;
+	if (reg_idx < 0 || reg_idx > intr->sde_irq_size) {
+		pr_err("invalid irq reg:%d irq:%d\n", reg_idx, irq_idx);
+		return;
+	}
+	
 	spin_lock_irqsave(&intr->irq_lock, irq_flags);
 	for (i = 0; i < ARRAY_SIZE(sde_intr_set); i++) {
 		/* Read interrupt status */
@@ -1107,7 +1112,8 @@ static void sde_hw_intr_clear_interrupt_status(struct sde_hw_intr *intr,
 		return;
 
 	spin_lock_irqsave(&intr->irq_lock, irq_flags);
-	sde_hw_intr_clear_intr_status_nolock(intr, irq_idx);
+	SDE_REG_WRITE(&intr->hw, intr->sde_irq_tbl[reg_idx].clr_off,
+			sde_irq_map[irq_idx].irq_mask);
 	spin_unlock_irqrestore(&intr->irq_lock, irq_flags);
 }
 
@@ -1132,9 +1138,6 @@ static u32 sde_hw_intr_get_intr_status_nolock(struct sde_hw_intr *intr,
 	if (intr_status && clear)
 		SDE_REG_WRITE(&intr->hw, sde_intr_set[reg_idx].clr_off,
 				intr_status);
-
-	/* ensure register writes go through */
-	wmb();
 
 	return intr_status;
 }

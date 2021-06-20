91e803372e1d55c0065240442363e46bc92f9e8c
Sultan Alsawaf <sultan@kerneltoast.com>
2021-06-20 23:02:29 +0800

    media: v4l2-ioctl: Use a larger on-stack video copy buffer
    We have a 16 KiB stack; a buffer of 1 KiB works perfectly fine in place
    of a small 128-byte buffer. This avoids dynamic memory allocation more
    often, improving performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2919,7 +2919,8 @@ long
 video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 	       v4l2_kioctl func)
 {
-	char	sbuf[128];
+	char	sbuf[SZ_4K] __aligned(8);
+	char    mbuf_onstack[SZ_512] __aligned(8);
 	void    *mbuf = NULL, *array_buf = NULL;
 	void	*parg = (void *)arg;
 	long	err  = -EINVAL;
@@ -2975,10 +2976,20 @@ video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 	has_array_args = err;
 
 	if (has_array_args) {
-		array_buf = kmalloc(array_size, GFP_KERNEL);
-		err = -ENOMEM;
-		if (array_buf == NULL)
-			goto out_array_args;
+		/*
+		 * When adding new types of array args, make sure that the
+		 * parent argument to ioctl (which contains the pointer to the
+		 * array) fits into sbuf (so that mbuf will still remain
+		 * unused up to here).
+		 */
+		if (array_size <= ARRAY_SIZE(mbuf_onstack)) {
+			array_buf = mbuf_onstack;
+		} else {
+			array_buf = kmalloc(array_size, GFP_KERNEL);
+			err = -ENOMEM;
+			if (NULL == array_buf)
+				goto out_array_args;
+		}
 		err = -EFAULT;
 		if (copy_from_user(array_buf, user_ptr, array_size))
 			goto out_array_args;
@@ -3021,9 +3032,10 @@ out_array_args:
 	}
 
 out:
-	kfree(array_buf);
-	kfree(mbuf);
+	if (array_buf != mbuf_onstack)
+		kfree(array_buf);
 	return err;
+	kfree(mbuf);
 }
 EXPORT_SYMBOL(video_usercopy);
 

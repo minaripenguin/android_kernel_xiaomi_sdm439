2fd4792bdd3df424f84f0e3326412a0b4df65b9e
mawrick26 <mawrick26@gmail.com>
2021-06-21 09:19:50 +0800

    mm: vmscan: If kswapd has been running too long, allow it to sleep
    Thanks MSF-Jarvis
    Under constant allocation pressure, kswapd can be in the situation where
    sleeping_prematurely() will always return true even if kswapd has been
    running a long time. Check if kswapd needs to be scheduled.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3171,6 +3171,10 @@ static bool pgdat_balanced(pg_data_t *pgdat, int order, int classzone_idx)
 	unsigned long mark = -1;
 	struct zone *zone;
 
+	/* If kswapd has been running too long, just sleep */
+	if (need_resched())
+		return false;
+		
 	for (i = 0; i <= classzone_idx; i++) {
 		zone = pgdat->node_zones + i;
 
